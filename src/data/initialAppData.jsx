acf2fe558588c15aad27ee2f7f1f80ab60460b91
Fernando Vargas <fvargas1644@gmail.com>
2024-09-08 01:45:51 -0500

    Cambiar datos por defecto de la App

--- src/data/initialAppData.jsx
+++ src/data/initialAppData.jsx
@@ -2,45 +2,30 @@ const initialAppData = {
     "lists": [
       {
         "id": "123e4567-e89b-12d3-a456-426614174000",
-        "name": "Lista 1 comprar la cena de ",
+        "name": "Tareas del proyecto de MiniTrello",
         "isVisible": true,
         "tasks": [
-          { "id": "123e4567-e89b-12d3-a456-426614174001", "name": "Comprar pavo", "isVisible": true },
-          { "id": "123e4567-e89b-12d3-a456-426614174002", "name": "Comprar pollo", "isVisible": true }
+          { "id": "123e4567-e89b-12d3-a456-426614174001", "name": "Subir nuestra aplicación a la nuve", "isVisible": true },
+          { "id": "123e4567-e89b-12d3-a456-426614174002", "name": "Actualizar los datos por defecto de la app", "isVisible": true },
+          { "id": "128e4567-e89b-12d3-a456-426614171726", "name": "Crear un articulo en relacionado a este proyecto", "isVisible": true }
         ]
       },
       {
         "id": "123e4567-e89b-12d3-a456-426614174003",
-        "name": "Lista 2 hacer ejercicio 6  al día",
+        "name": "Tareas para el día de mañana",
         "isVisible": true,
         "tasks": [
-          { "id": "567e4567-e89b-12d3-a456-426614174192", "name": "Correr 30 minutos", "isVisible": true },
-          { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer 100 lagartijas", "isVisible": true }
+          { "id": "567e4567-e89b-12d3-a456-426614174192", "name": "Terminar informe", "isVisible": true },
+          { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer ejerecicio después de las 4 PM", "isVisible": true }
         ]
       },
       {
         "id": "123e4567-e89b-12d3-a456-426614174004",
-        "name": "Lista 3 salir el fin de semana próximo",
+        "name": "Dame Click o Arrastrame",
         "isVisible": true,
-        "tasks": []
-      },
-      {
-        "id": "123e4567-e89b-12d3-a456-426614174005",
-        "name": "Lista 4 Pero si hacemos un texto quizás estemos dando por alto ciertas cosas importantes",
-        "isVisible": true,
-        "tasks": []
-      },
-      {
-        "id": "123e4567-e89b-12d3-a456-426614174006",
-        "name": "Lista 5",
-        "isVisible": true,
-        "tasks": []
-      },
-      {
-        "id": "123e4567-e89b-12d3-a456-426614174007",
-        "name": "Lista 6",
-        "isVisible": true,
-        "tasks": []
+        "tasks": [
+          { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Dame Click o Arrastrame", "isVisible": true }
+        ]
       }
     ]
   }
