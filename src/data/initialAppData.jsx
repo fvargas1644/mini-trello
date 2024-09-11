61250fe45eeafb266e40612fb8a1ebd58f2ded9a
Fernando Vargas <fvargas1644@gmail.com>
2024-09-11 12:06:36 -0500

    Modificar los datos iniciales de la App

--- src/data/initialAppData.jsx
+++ src/data/initialAppData.jsx
@@ -7,7 +7,7 @@ const initialAppData = {
       "tasks": [
         { "id": "123e4567-e89b-12d3-a456-426614174001", "name": "Subir nuestra aplicación a la nuve", "isVisible": true },
         { "id": "123e4567-e89b-12d3-a456-426614174002", "name": "Actualizar los datos por defecto de la app", "isVisible": true },
-        { "id": "128e4567-e89b-12d3-a456-426614171726", "name": "Crear un articulo en relacionado a este proyecto", "isVisible": true }
+        { "id": "128e4567-e89b-12d3-a456-426614171726", "name": "Crear un artículo relacionado a este proyecto", "isVisible": true }
       ]
     },
     {
@@ -16,15 +16,15 @@ const initialAppData = {
       "isVisible": true,
       "tasks": [
         { "id": "567e4567-e89b-12d3-a456-426614174192", "name": "Terminar informe", "isVisible": true },
-        { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer ejerecicio después de las 4 PM", "isVisible": true }
+        { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer ejercicio después de las 4 PM", "isVisible": true }
       ]
     },
     {
       "id": "123e4567-e89b-12d3-a456-426614174004",
-      "name": "Dame Click o Arrastrame",
+      "name": "Dame Click o Arrástrame",
       "isVisible": true,
       "tasks": [
-        { "id": "12754567-e89b-12d3-a456-426614173456", "name": "Dame Click o Arrastrame", "isVisible": true }
+        { "id": "12754567-e89b-12d3-a456-426614173456", "name": "Dame Click o Arrástrame", "isVisible": true }
       ]
     }
   ]
