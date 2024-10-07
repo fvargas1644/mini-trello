9bed9ef821ad5984a26eefed735199d86f13f89a
Fernando Vargas <fvargas1644@gmail.com>
2024-10-07 10:46:36 -0500

    Agregar test para el customHook useListCard

--- src/test/list/ListCard.test.jsx
+++ src/test/list/ListCard.test.jsx
@@ -1,7 +1,8 @@
 import { beforeEach, describe, expect, test } from "vitest";
 import ListCard from "../../components/list/ListCard.jsx";
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, screen, fireEvent, act, renderHook } from "@testing-library/react";
 import { AppDataContext, DragDataContext } from '../../context/AppContext.jsx';
+import { useListCard } from "../../hooks/list/useListCard.jsx";
 
 const setAppDataMock = vi.fn();
 const setDragDataMock = vi.fn();
@@ -20,6 +21,16 @@ const appDataContextMock = {
                     { "id": "567e4567-e89b-12d3-a456-426614174192", "name": "Terminar informe", "isVisible": true },
                     { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer ejercicio después de las 4 PM", "isVisible": true }
                 ]
+            },
+            {
+                "id": "123e4567-e89b-12d3-a456-426614174000",
+                "name": "Tareas del proyecto de MiniTrello",
+                "isVisible": true,
+                "tasks": [
+                    { "id": "123e4567-e89b-12d3-a456-426614174001", "name": "Subir nuestra aplicación a la nube", "isVisible": true },
+                    { "id": "123e4567-e89b-12d3-a456-426614174002", "name": "Actualizar los datos por defecto de la app", "isVisible": true },
+                    { "id": "128e4567-e89b-12d3-a456-426614171726", "name": "Crear un artículo relacionado a este proyecto", "isVisible": true }
+                ]
             }
         ]
     }
@@ -32,15 +43,28 @@ const dragDataContextMock = {
         draggedTaskIndex: null,
         dragTaskData: undefined,
         draggedTask: false,
-        draggedListIndex: null, 
-        dragItemType: null, 
-        activeGhostTask: { 
+        draggedListIndex: null,
+        dragItemType: null,
+        activeGhostTask: {
             active: false,
             listId: null,
             toListIndex: null,
         }
     }
 }
+
+// Mock Setup
+const renderListCard = (listId, listIndex) => {
+    return render(
+        <AppDataContext.Provider value={appDataContextMock}>
+            <DragDataContext.Provider value={dragDataContextMock}>
+                <ListCard listId={listId} listIndex={listIndex}>
+                    <h1>Children</h1>
+                </ListCard>
+            </DragDataContext.Provider>
+        </AppDataContext.Provider>
+    );
+};
 
 describe('ListCard', () => {
     let containerComponent;
@@ -50,17 +74,7 @@ describe('ListCard', () => {
         setAppDataMock.mockClear();
         setDragDataMock.mockClear();
 
-        const renderResul = render(
-            <AppDataContext.Provider value={appDataContextMock}>
-                <DragDataContext.Provider value={dragDataContextMock}>
-                    <ListCard listId={listId} listIndex={listIndex}>
-                        <h1>Children</h1>
-                    </ListCard>
-                </DragDataContext.Provider>
-            </AppDataContext.Provider>
-        );
-
-        containerComponent = renderResul.container;
+        containerComponent = renderListCard(listId, listIndex).container;
     });
 
     test('Should be a article', () => {
@@ -71,41 +85,60 @@ describe('ListCard', () => {
         expect(screen.getByText('Children')).toBeInTheDocument();
     });
 
-    test('', async ()=>{
-        //await fireEvent.animationEnd(containerComponent.querySelector('.mt-list-container'));
-        //expect(setAppDataMock).toBeCalled();
-    });
+});
 
-    test('', async ()=>{
 
-        let dataDrag = {
-            updateDragData: setDragDataMock,
-            dragData: {
-                draggedTaskIndex: 1,
-                dragTaskData: undefined,
-                draggedTask: false,
-                draggedListIndex: null, 
-                dragItemType: 'list', 
-                activeGhostTask: { 
-                    active: false,
-                    listId: null,
-                    toListIndex: null,
-                }
-            }
-        }
 
-        await render(
+describe('useListCard', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = ({ children }) =>
             <AppDataContext.Provider value={appDataContextMock}>
-                <DragDataContext.Provider value={dataDrag}>
-                    <ListCard listId={listId} listIndex={listIndex}>
-                        <h1>Children</h1>
-                    </ListCard>
+                <DragDataContext.Provider value={dragDataContextMock}>
+                    {children}
                 </DragDataContext.Provider>
             </AppDataContext.Provider>
-        );
+    });
 
-        await fireEvent.dragOver(containerComponent.querySelector('.mt-list-container'));
+    let listIndex = 0;
+    test('Should ', async () => {
+        const { result } = renderHook(() => useListCard({ listId, listIndex }), { wrapper });
+        const event = {
+            animationName: 'list-fade-out'
+        }
+        await act(() => {
+            result.current.listAnimationEnd(event)
+        });
 
-        //expect(setDragDataMock).toBeCalled();
+        expect(setAppDataMock).toHaveBeenCalledWith(expect.objectContaining({
+            lists: expect.arrayContaining([
+                expect.objectContaining({ id: listId })
+            ])
+        }));
+
+        expect(setAppDataMock).toHaveBeenCalledWith({
+            lists: [
+                {
+                    "id": listId,
+                    "name": listName,
+                    "isVisible": false, // Cambio a false
+                    "tasks": [
+                        { "id": "567e4567-e89b-12d3-a456-426614174192", "name": "Terminar informe", "isVisible": true },
+                        { "id": "127e4567-e89b-12d3-a456-426614174129", "name": "Hacer ejercicio después de las 4 PM", "isVisible": true }
+                    ]
+                }, 
+                {
+                    "id": "123e4567-e89b-12d3-a456-426614174000",
+                    "name": "Tareas del proyecto de MiniTrello",
+                    "isVisible": true,
+                    "tasks": [
+                        { "id": "123e4567-e89b-12d3-a456-426614174001", "name": "Subir nuestra aplicación a la nube", "isVisible": true },
+                        { "id": "123e4567-e89b-12d3-a456-426614174002", "name": "Actualizar los datos por defecto de la app", "isVisible": true },
+                        { "id": "128e4567-e89b-12d3-a456-426614171726", "name": "Crear un artículo relacionado a este proyecto", "isVisible": true }
+                    ]
+                }
+            ]
+        });
     });
 });
